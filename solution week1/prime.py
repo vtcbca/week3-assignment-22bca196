a8294963ae30a07d406214a358f908a77967ce6c
22bca196 <127926379+22bca196@users.noreply.github.com>
2023-07-14 10:42:05 +0530

    script 1
    script 1

---
+++ solution week1/prime.py
@@ -0,0 +1,11 @@
+# Write a python script to enter any number and check its prime or not.
+
+def check_prime(no):
+    for i in range(2,no,no):
+        if no%i==0:
+            print(f'{no}is not prime number')
+            break
+        else:
+            print(f'{no}is prime number')
+no=int(input("Enter Any Number:"))
+check_prime(no)
